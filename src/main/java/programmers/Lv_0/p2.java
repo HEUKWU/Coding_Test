d9ea8859f4660b8bbf1d797dc2071e36d4ad0eb0
HEUKWU <yoo4068@gmail.com>
2023-01-20 23:04:08 +0900

    옷가게 할인 받기

---
+++ src/main/java/programmers/Lv_0/p2.java
@@ -0,0 +1,20 @@
+package programmers.Lv_0;
+
+public class p2 {
+    public static void main(String[] args) {
+        System.out.println(new Solution2().solution(150000));
+    }
+}
+
+class Solution2 {
+    public int solution(int price) {
+        if (100000 <= price && price < 300000) {
+            price *= 0.95;
+        } else if (300000 <= price && price < 500000) {
+            price *= 0.9;
+        } else if (500000 <= price) {
+            price *= 0.8;
+        }
+        return price;
+    }
+}
