a08cb79977e8bc7b942cdcd0db9d89b5323827e0
HEUKWU <yoo4068@gmail.com>
2023-01-21 13:12:14 +0900

    가장 큰 수 찾기

---
+++ src/main/java/programmers/Lv_0/p8.java
@@ -0,0 +1,36 @@
+package programmers.Lv_0;
+
+import java.util.Arrays;
+
+public class p8 {
+    public static void main(String[] args) {
+        int[] a = {9, 10, 11, 8};
+        System.out.println(Arrays.toString(solution(a)));
+    }
+    public static int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = 0;
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max) {
+                a = i;
+            }
+        }
+        answer[0] = max;
+        answer[1] = a;
+        return answer;
+//        int[] answer = new int[2];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > answer[0]) {
+//                answer[0] = array[i];
+//                answer[1] = i;
+//            }
+//        }
+//        return answer;
+    }
+}
