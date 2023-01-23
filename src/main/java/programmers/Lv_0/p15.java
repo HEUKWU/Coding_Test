6eb6b0c1adde7fc6278adac151e4aa80ff217b15
HEUKWU <yoo4068@gmail.com>
2023-01-23 11:24:27 +0900

    숫자 찾기

---
+++ src/main/java/programmers/Lv_0/p15.java
@@ -0,0 +1,26 @@
+package programmers.Lv_0;
+
+public class p15 {
+    public static void main(String[] args) {
+        System.out.println(solution(123456, 7));
+    }
+
+    public static int solution(int num, int k) {
+        int length = (int) (Math.log10(num) + 1);
+        int[] a = new int[length];
+        for (int i = 0; i < length; i++) {
+            a[length - 1 - i] = num % 10;
+            num /= 10;
+        }
+        int answer = 0;
+        for (int i = 0; i < length; i++) {
+            if (k == a[i]) {
+                answer = i + 1;
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
