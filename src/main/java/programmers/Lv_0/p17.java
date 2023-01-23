ded871273bc61f0ee32164d8cd7ce3bc0b05de77
HEUKWU <yoo4068@gmail.com>
2023-01-23 13:16:17 +0900

    합성수 찾기

---
+++ src/main/java/programmers/Lv_0/p17.java
@@ -0,0 +1,24 @@
+package programmers.Lv_0;
+
+public class p17 {
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+
+    public static int solution(int n) {
+        int a = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    a++;
+                }
+            }
+            if (a >= 3) {
+                count++;
+            }
+            a = 0;
+        }
+        return count;
+    }
+}
