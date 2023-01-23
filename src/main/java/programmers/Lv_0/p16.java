a6c1ac6a8df70c9d6a9d5d86de33ccbae06096e0
HEUKWU <yoo4068@gmail.com>
2023-01-23 13:07:26 +0900

    문자열 정렬하기 (2)

---
+++ src/main/java/programmers/Lv_0/p16.java
@@ -0,0 +1,16 @@
+package programmers.Lv_0;
+
+import java.util.Arrays;
+
+public class p16 {
+    public static void main(String[] args) {
+        System.out.println(solution("Python"));
+    }
+
+    public static String solution(String my_string) {
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        my_string = new String(c);
+        return my_string;
+    }
+}
