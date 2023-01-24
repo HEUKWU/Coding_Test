49b71d1d985be3019cbde22e876aeb814bb4122b
HEUKWU <yoo4068@gmail.com>
2023-01-24 16:49:42 +0900

    팩토리얼

---
+++ src/main/java/programmers/Lv_0/p19.java
@@ -0,0 +1,29 @@
+package programmers.Lv_0;
+
+public class p19 {
+    public static void main(String[] args) {
+        System.out.println(solution(24));
+    }
+
+    public static int solution(int n) {
+        int sum = 1;
+        for (int i = 1; true; i++) {
+            sum *= i;
+            if (sum == n) {
+                sum = i;
+                break;
+            } else if (sum > n) {
+                sum = i - 1;
+                break;
+            }
+        }
+        return sum;
+//        int sum = 1;
+//        int i = 1;
+//        while (sum <= n) {
+//            i++;
+//            sum *= i;
+//        }
+//        return i - 1;
+    }
+}
