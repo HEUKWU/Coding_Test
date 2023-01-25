eae4b7a2a456a83702b2fb583a56a0d8f3c30620
HEUKWU <yoo4068@gmail.com>
2023-01-25 10:45:45 +0900

    진료순서 정하기 *

---
+++ src/main/java/programmers/Lv_0/p24.java
@@ -0,0 +1,24 @@
+package programmers.Lv_0;
+
+import java.util.Arrays;
+
+public class p24 {
+    public static void main(String[] args) {
+        int[] a = {30, 10, 23, 6, 100};
+        System.out.println(Arrays.toString(solution(a)));
+    }
+
+    public static int[] solution(int[] emergency) {
+        int[] a = emergency.clone();
+        int[] answer = new int[emergency.length];
+        Arrays.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (emergency[i] == a[j]) {
+                    answer[i] = a.length - j;
+                }
+            }
+        }
+        return answer;
+    }
+}
