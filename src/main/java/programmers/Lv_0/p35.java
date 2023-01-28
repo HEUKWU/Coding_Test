43ba91cba6f0f8dcb3e3f5f6958e4ed01dc5997a
HEUKWU <yoo4068@gmail.com>
2023-01-28 11:59:29 +0900

    잘라서 배열로 저장하기*

---
+++ src/main/java/programmers/Lv_0/p35.java
@@ -0,0 +1,22 @@
+package programmers.Lv_0;
+
+import java.util.Arrays;
+
+public class p35 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("abc1Addfggg4556b", 6)));
+    }
+
+    public static String[] solution(String my_str, int n) {
+        int resultCnt = (my_str.length() + n - 1) / n;
+        String[] answer = new String[resultCnt];
+
+        for (int i = 0; i < resultCnt; i++) {
+            int start = n * i;
+            int end = Math.min(start + n, my_str.length());
+            answer[i] = my_str.substring(start, end);
+        }
+
+        return answer;
+    }
+}
