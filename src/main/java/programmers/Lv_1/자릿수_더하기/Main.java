ee8caf9f8ebc95a7a03e63ed4a4faa677b53b1a3
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-06-04 01:42:33 +0900

    자릿수 더하기

--- src/main/java/programmers/Lv_1/자릿수_더하기/Main.java
+++ src/main/java/programmers/Lv_1/자릿수_더하기/Main.java
@@ -1,7 +1,20 @@
 package programmers.Lv_1.자릿수_더하기;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        while(n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
