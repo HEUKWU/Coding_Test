13bfb510ce1a0bed3aa9136c6e3753766922ce3c
heukwu <yoo4068@gmail.com>
2023-06-08 10:24:17 +0900

    약수의 개수와 덧셈

--- src/main/java/programmers/Lv_1/약수의_개수와_덧셈/Main.java
+++ src/main/java/programmers/Lv_1/약수의_개수와_덧셈/Main.java
@@ -2,15 +2,35 @@ package programmers.Lv_1.약수의_개수와_덧셈;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(solution(12));
+        Solution solution = new Solution();
+        System.out.println(solution.solution(13, 17));
     }
-    public static int solution(int n) {
+}
+
+class Solution {
+    public int solution(int left, int right) {
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
+
+        for (int i = left; i <= right; i++) {
+            if (count(i)) {
                 sum += i;
+            } else {
+                sum -= i;
             }
         }
+
         return sum;
+    }
+
+    static boolean count(int i) {
+        int count = 0;
+
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count++;
+            }
+        }
+
+        return count % 2 == 0;
     }
 }
