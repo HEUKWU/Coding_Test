66442c04c6e76a5b30522ec91531014713bd057b
heukwu <yoo4068@gmail.com>
2023-06-09 09:56:41 +0900

    문자열 내림차순으로 배치하기

--- src/main/java/programmers/Lv_1/문자열_내림차순으로_배치하기/Main.java
+++ src/main/java/programmers/Lv_1/문자열_내림차순으로_배치하기/Main.java
@@ -4,33 +4,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(solution("Zbcdefg"));
+        System.out.println(solution("bcaA"));
     }
     public static String solution(String s) {
-//        String u = "";
-//        String l = "";
-//        for (int i = 0; i < s.length(); i++) {
-//            if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
-//                l += s.charAt(i);
-//            } else {
-//                u += s.charAt(i);
-//            }
-//        }
-//        String[] us = u.split("");
-//        String[] ls = l.split("");
-//        Arrays.sort(us);
-//        Arrays.sort(ls);
-//        StringBuilder ub = new StringBuilder();
-//        for (String i : us) {
-//            ub.append(i);
-//        }
-//        StringBuilder lb = new StringBuilder();
-//        for (String i : ls) {
-//            lb.append(i);
-//        }
-//        return String.valueOf(lb.reverse().append(ub.reverse()));
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        return new StringBuilder(new String(chars)).reverse().toString();
+        StringBuilder sb = new StringBuilder();
+        char[] c = s.toCharArray();
+
+        Arrays.sort(c);
+
+        for (int i = c.length - 1; i >= 0; i--) {
+            sb.append(c[i]);
+        }
+
+        return sb.toString();
     }
 }
