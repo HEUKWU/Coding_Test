7cd96406a72c5a8c1d6f3277e979b7fbb772a54a
heukwu <yoo4068@gmail.com>
2023-06-15 09:02:26 +0900

    숫자 문자열과 영단어

--- src/main/java/programmers/Lv_1/숫자_문자열과_영단어/Main.java
+++ src/main/java/programmers/Lv_1/숫자_문자열과_영단어/Main.java
@@ -9,16 +9,11 @@ public class Main {
     }
 
     public static int solution(String s) {
-        s = s.replace("zero", "0");
-        s = s.replace("one", "1");
-        s = s.replace("two", "2");
-        s = s.replace("three", "3");
-        s = s.replace("four", "4");
-        s = s.replace("five", "5");
-        s = s.replace("six", "6");
-        s = s.replace("seven", "7");
-        s = s.replace("eight", "8");
-        s = s.replace("nine", "9");
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < arr.length; i++) {
+            s = s.replace(arr[i], String.valueOf(i));
+        }
 
         return Integer.parseInt(s);
     }
