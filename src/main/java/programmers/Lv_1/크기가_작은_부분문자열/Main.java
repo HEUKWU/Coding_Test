e290297bf3a36a73ee1783a4124369596c39b155
heukwu <yoo4068@gmail.com>
2023-06-20 13:47:33 +0900

    크기가 작은 부분문자열

--- src/main/java/programmers/Lv_1/크기가_작은_부분문자열/Main.java
+++ src/main/java/programmers/Lv_1/크기가_작은_부분문자열/Main.java
@@ -10,11 +10,8 @@ public class Main {
     static int solution(String t, String p) {
         int count = 0;
         for (int i = 0; i < t.length() - p.length() + 1; i++) {
-            StringBuilder sb = new StringBuilder().append(t.charAt(i));
-            for (int j = 0, k = i + 1; j < p.length() - 1; j++, k++) {
-                sb.append(t.charAt(k));
-            }
-            if (Long.parseLong(sb.toString()) <= Long.parseLong(p)) {
+            long n = Long.parseLong(t.substring(i, i + p.length()));
+            if (n <= Long.parseLong(p)) {
                 count++;
             }
         }
