4bb3b3d01f49a3f6881a3aba2d8ad7df5afe290c
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-06-23 00:02:36 +0900

    최소, 최대

--- src/main/java/BOJ/B3/n10818/Main.java
+++ src/main/java/BOJ/B3/n10818/Main.java
@@ -1,7 +1,32 @@
 package BOJ.B3.n10818;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int[] num = new int[a];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = num[0];
+        int max = num[0];
+        
+        for (int i : num) {
+            if (min > i) {
+                min = i;
+            }
+            if (max < i) {
+                max = i;
+            }
+        }
+        System.out.println(min + " " + max);
     }
 }
