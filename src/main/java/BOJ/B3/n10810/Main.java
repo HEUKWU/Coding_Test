74d74947a42d6463aaddc6c57b164d88c2b2b2a0
heukwu <yoo4068@gmail.com>
2023-06-22 18:02:19 +0900

    init

---
+++ src/main/java/BOJ/B3/n10810/Main.java
@@ -0,0 +1,7 @@
+package BOJ.B3.n10810;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/BOJ/B3/n10818/Main.java
@@ -0,0 +1,7 @@
+package BOJ.B3.n10818;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
