540f989d0f2f2f361ab62ad6407e4761664c401d
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-06-24 00:58:50 +0900

    최댓값

--- src/main/java/BOJ/B3/n2562/Main.java
+++ src/main/java/BOJ/B3/n2562/Main.java
@@ -1,7 +1,25 @@
 package BOJ.B3.n2562;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int n = 0;
+
+        for (int i = 0; i < 9; i++) {
+            int a = Integer.parseInt(br.readLine());
+
+            if (a > max) {
+                max = a;
+                n = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(n);
     }
 }
