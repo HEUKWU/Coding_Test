01303def817ca4d1784f576574a4a3a41f78ee3e
heukwu <yoo4068@gmail.com>
2023-06-27 11:33:40 +0900

    소수 만들기

--- src/main/java/programmers/Lv_1/소수_만들기/Main.java
+++ src/main/java/programmers/Lv_1/소수_만들기/Main.java
@@ -1,32 +1,35 @@
 package programmers.Lv_1.소수_만들기;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{1,2,7,6,4}));
+        int[] a = {1,2,3,4};
+        int[] b = {1, 2, 7, 6, 4};
+        System.out.println(solution(a));
+        System.out.println(solution(b));
     }
-    public static int solution(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
+
+    static int solution(int[] nums) {
+        int count = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    list.add(nums[i] + nums[j] + nums[k]);
+                    count = num(nums[i] + nums[j] + nums[k], count);
                 }
             }
         }
-        int answer = 0;
-        for (Integer integer : list) {
-            int a = 0;
-            for (int j = 1; j <= integer; j++) {
-                if (integer % j == 0) {
-                    a++;
-                }
-            }
-            if (a == 2) {
-                answer++;
+
+        return count;
+    }
+
+    static int num(int a, int count) {
+        int c = 0;
+
+        for (int i = 2; i <= a; i++) {
+            if (a % i == 0) {
+                c++;
             }
         }
-        return answer;
+
+        return c == 1 ? count + 1 : count;
     }
 }
