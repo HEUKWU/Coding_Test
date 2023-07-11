7fe2deffcdc3e168bfdd892df93c13903c2604ca
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-07-11 12:21:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 316 ms, Memory: 86716 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int dev = 666;
+        int count = 1;
+
+        while (count != n) {
+            dev++;
+            if (String.valueOf(dev).contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.println(dev);
+    }
+}
