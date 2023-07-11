8e94abd25ab7d7ab453be4937903f6f02f557223
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-07-11 19:48:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (true) {
+            if (n % 5 == 0) {
+                System.out.println(n / 5 + count);
+                break;
+            } else if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+            n -= 3;
+            count++;
+        }
+    }
+}
