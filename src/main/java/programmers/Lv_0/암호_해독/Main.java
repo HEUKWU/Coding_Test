f702fb55b224d01f2cd60460149eb8514c1961c1
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-07-30 06:57:14 +0900

    암호 해독

--- src/main/java/programmers/Lv_0/암호_해독/Main.java
+++ src/main/java/programmers/Lv_0/암호_해독/Main.java
@@ -1,12 +1,11 @@
 package programmers.Lv_0.암호_해독;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = code; i <= cipher.length(); i += code) {
+            answer.append(cipher.charAt(i - 1));
+        }
+        return answer.toString();
     }
 }
