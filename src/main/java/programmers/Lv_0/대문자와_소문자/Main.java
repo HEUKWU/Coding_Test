8318d5910517569aebed0b691432ffd91b9e7d2d
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-07-30 06:58:22 +0900

    대문자와 소문자

--- src/main/java/programmers/Lv_0/대문자와_소문자/Main.java
+++ src/main/java/programmers/Lv_0/대문자와_소문자/Main.java
@@ -1,12 +1,16 @@
 package programmers.Lv_0.대문자와_소문자;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            char a = my_string.charAt(i);
+            if ('a' <= a && a <= 'z') {
+                sb.append((a + "").toUpperCase());
+            } else {
+                sb.append((a + "").toLowerCase());
+            }
+        }
+        return sb.toString();
     }
 }
