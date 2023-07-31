85d9d628bf8b9aaab26d2f9d991efa783ded71a4
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-07-31 10:14:02 +0900

    n의 배수 고르기

--- src/main/java/programmers/Lv_0/n의_배수_고르기/Main.java
+++ src/main/java/programmers/Lv_0/n의_배수_고르기/Main.java
@@ -1,12 +1,19 @@
 package programmers.Lv_0.n의_배수_고르기;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int count = 0;
+        int j = 0;
+        int[] a = new int[numlist.length];
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                count++;
+                a[j] = numlist[i];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(a, 0, count);
     }
 }
