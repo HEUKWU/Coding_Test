6f38d043269caa9a7013f08a2487e2280209a571
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-08-02 10:02:22 +0900

    문자열 정렬하기 (1)

--- src/main/java/programmers/Lv_0/문자열_정렬하기_1/Main.java
+++ src/main/java/programmers/Lv_0/문자열_정렬하기_1/Main.java
@@ -1,12 +1,22 @@
 package programmers.Lv_0.문자열_정렬하기_1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public int[] solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            char a = my_string.charAt(i);
+            if ('0' <= a && a <= '9') {
+                sb.append(a);
+            }
+        }
+        int[] answer = new int[sb.length()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = sb.toString().charAt(i) - '0';
+        }
+        Arrays.sort(answer);
 
+        return answer;
     }
 }
