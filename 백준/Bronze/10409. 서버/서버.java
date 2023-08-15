602ae413af76f7fa537b83129c1b0b0b1882c1ee
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-08-15 20:15:41 +0900

    [Bronze III] Title: 서버, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/10409. 서버/서버.java
+++ 백준/Bronze/10409. 서버/서버.java
@@ -7,20 +7,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int sum = 0;
-        int count = 0;
+
         int n = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int sum  = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int ac = Integer.parseInt(st.nextToken());
-            if (t < sum + ac) {
-                System.out.println(count);
-                return;
+            int a = Integer.parseInt(st.nextToken());
+            sum += a;
+            if (sum > t) {
+                break;
             }
-            sum += ac;
             count++;
         }
+
         System.out.println(count);
     }
 }
