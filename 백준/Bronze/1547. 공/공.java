df62d8ff7e5f0e832f19cf84c0e3dfb46d555b49
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-08-16 07:35:22 +0900

    [Bronze III] Title: 공, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == num) {
+                num = b;
+            } else if (b == num) {
+                num = a;
+            }
+        }
+        System.out.println(num);
+    }
+}
