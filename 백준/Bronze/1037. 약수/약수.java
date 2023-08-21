c20ff2b5a1a438c3413d8c9fb8683c3b6a0e1e7f
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-08-21 20:07:24 +0900

    [Bronze I] Title: 약수, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println((int) Math.pow(Integer.parseInt(br.readLine()), 2));
+            return;
+        }
+
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        System.out.println(a[0] * a[n - 1]);
+    }
+}
