a3bbe0be23c9c52fa3e82514c410c2a5ae9efd1c
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-08-22 19:14:06 +0900

    [Silver V] Title: 숫자 카드, Time: 808 ms, Memory: 175656 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] cards = new int[20000001];
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cards[Integer.parseInt(st.nextToken()) + 10000000]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            sb.append(cards[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
