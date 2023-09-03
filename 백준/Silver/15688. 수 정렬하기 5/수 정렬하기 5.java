a68d069d1718eaaf9a8e7266f3f5d422afe37352
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-09-03 12:31:43 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 18104 ms, Memory: 101756 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[2000001];
+
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                while (arr[i]-- != 0) {
+                    sb.append(i - 1000000).append('\n');
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
