064256a0e3900b1db729df73d8c6b804a97e0c8c
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-09-04 21:43:56 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -11,26 +11,25 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = 0;
+        int sum = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
                     int a = arr[i] + arr[j] + arr[k];
-                    if (a <= m && max < a) {
-                        max = a;
+                    if (a <= m && sum < a) {
+                        sum = a;
                     }
                 }
             }
         }
 
-        System.out.println(max);
+        System.out.println(sum);
     }
 }
