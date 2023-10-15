25a60ccd47969e7153e2ae06c52e526b97d70fd8
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-10-15 15:37:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static int dx[] = {0,0,1,-1};
+    private static int dy[] = {1,-1,0,0};
+    private static int[] aparts = new int[25*25];
+    private static int n;
+    private static int apartNum = 0;
+    private static boolean[][] visited = new boolean[25][25];
+    private static int[][] map = new int[25][25];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    apartNum++;
+                    dfs(i, j);
+                }
+            }
+        }
+
+        Arrays.sort(aparts);
+        System.out.println(apartNum);
+
+        for (int apart : aparts) {
+            if (apart != 0) {
+                System.out.println(apart);
+            }
+        }
+    }
+
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+        aparts[apartNum]++;
+
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
