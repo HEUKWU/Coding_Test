237c3dcc667a730a8b6982cda28a376c26ee1c89
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-11-05 20:23:47 +0900

    [Bronze I] Title: N번째 큰 수, Time: 208 ms, Memory: 16308 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int[] arr = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < 10; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            System.out.println(arr[7]);
+        }
+    }
+}
