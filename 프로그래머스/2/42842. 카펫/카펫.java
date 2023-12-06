7fcefc0d785bd347d7fcf5e97ab0194d3b6b6cbf
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2023-12-06 12:58:01 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,20 +2,15 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int area = brown + yellow;
 
-        int x = 0;
-        int y = 0;
-        for (int i = 2; i < area; i++) {
+        for (int i = 2; i <= Math.sqrt(area); i++) {
             if (area % i == 0) {
                 int j = area / i;
-                if (area - ((j - 2) * (i - 2)) == brown) {
-                    x = Math.max(i, j);
-                    y = Math.min(i, j);
-
-                    break;
+                if ((j - 2) * (i - 2) == yellow) {
+                    return new int[]{j, i};
                 }
             }
         }
 
-        return new int[]{x, y};
+        return null;
     }
 }
