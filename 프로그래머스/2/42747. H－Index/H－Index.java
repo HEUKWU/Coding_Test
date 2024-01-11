0361183decacfdc89df13d7e214de43b7f6dc83c
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-01-11 15:37:20 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,16 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        
-        int answer = 0;
-        
-        for(int i = 0; i < citations.length; i++) {
-			int h = citations.length - i;
-			
-			if(citations[i] >= h) {
-				answer = h;
-				break;
-			}
-		}
-        
-        
-        
-        return answer;
+
+        int n = citations.length;
+        int max = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int min = Math.min(citations[i], n - i);
+            max = Math.max(max, min);
+        }
+
+        return max;
     }
 }
