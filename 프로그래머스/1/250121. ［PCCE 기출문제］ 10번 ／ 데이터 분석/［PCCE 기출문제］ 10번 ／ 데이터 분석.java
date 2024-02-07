1ceea57d506f36ae808e7e3e9673dc4a36438970
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-02-07 17:47:57 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 8.10 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,50 +1,18 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int extN = 0;
-        switch (ext) {
-            case "date":
-                extN = 1;
-                break;
-            case "maximum":
-                extN = 2;
-                break;
-            case "remain":
-                extN = 3;
-                break;
-        }
+        String[] type = {"code", "date", "maximum", "remain"};
+        List<String> list = Arrays.asList(type);
 
-        int sort = 0;
-        switch (sort_by) {
-            case "date":
-                sort = 1;
-                break;
-            case "maximum":
-                sort = 2;
-                break;
-            case "remain":
-                sort = 3;
-                break;
-        }
+        int extN = list.indexOf(ext);
+        int sortN = list.indexOf(sort_by);
 
-        List<int[]> list = new ArrayList<>();
-        for (int[] datum : data) {
-            if (datum[extN] < val_ext) {
-                list.add(datum);
-            }
-        }
-
-        int sortN = sort;
-        list.sort(Comparator.comparingInt(a -> a[sortN]));
-
-        int[][] newData = new int[list.size()][4];
-        for (int i = 0; i < list.size(); i++) {
-            newData[i] = list.get(i);
-        }
-
-        return newData;
+        return Arrays.stream(data)
+                .filter(a -> a[extN] < val_ext)
+                .sorted(Comparator.comparingInt(a -> a[sortN]))
+                .toArray(int[][]::new);
     }
 }
