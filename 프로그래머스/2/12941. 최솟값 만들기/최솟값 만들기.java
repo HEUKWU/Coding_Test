ae914715262949421913f106c171bcbe781e7d20
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-02-27 22:10:14 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.20 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(int []A, int []B) {
         Arrays.sort(A);
         Arrays.sort(B);
-
+        
         int sum = 0;
         for (int i = 0; i < A.length; i++) {
-            sum += A[i] * B[A.length - 1 - i];
+            sum += A[i] * B[A.length - i - 1];
         }
 
         return sum;
