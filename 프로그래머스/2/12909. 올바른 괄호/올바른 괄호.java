61f3063744d53cdef7d6a2798b9cfe6c8f4e64a6
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-02-28 21:51:20 +0900

    [level 2] Title: 올바른 괄호, Time: 20.69 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,27 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        int count = 0;
+        if (s.charAt(0) == ')') {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                count++;
-            } else {
-                count--;
+            char c = s.charAt(i);
+            if (stack.empty()) {
+                stack.push(c);
+                continue;
             }
 
-            if (count < 0) {
-                return false;
+            if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
 
-        return count == 0;
+        return stack.empty();
     }
 }
