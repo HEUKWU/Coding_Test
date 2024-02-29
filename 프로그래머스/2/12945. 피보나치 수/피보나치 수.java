f80935d7ec30182b6bbd8a03be2be7321881d9fa
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-02-29 20:05:09 +0900

    [level 2] Title: 피보나치 수, Time: 3.69 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,17 @@
 class Solution {
     public int solution(int n) {
-        int[] num = new int[n + 1];
-        num[0] = 0;
-        num[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            num[i] = (num[i - 1] + num[i - 2]) % 1234567;
+        if (n == 1 || n == 2) {
+            return 1;
         }
 
-        return num[n];
+        int[] a = new int[n + 1];
+        a[1] = 1;
+        a[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            a[i] = (a[i - 1] + a[i - 2]) % 1234567;
+        }
+
+        return a[n];
     }
 }
