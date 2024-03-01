1b6d9ff765510d06d26afe6ab2d62b8e882f5f60
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-01 11:24:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,33 +1,23 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         list.add(words[0]);
 
-        int a = 2;
-        int b = 1;
         for (int i = 1; i < words.length; i++) {
             String word = words[i];
-            String prev = words[i - 1];
-
-            if (word.charAt(0) != prev.charAt(prev.length() - 1)) {
-                return new int[]{a, b};
-            }
             if (list.contains(word)) {
-                return new int[]{a, b};
+                return new int[]{(i % n + 1), (i / n + 1)};
             }
-            
+
+            String prev = words[i - 1];
+            if (prev.charAt(prev.length() - 1) != word.charAt(0)) {
+                return new int[]{(i % n + 1), (i / n + 1)};
+
+            }
+
             list.add(word);
-
-            a++;
-            if (a == n + 1) {
-                a = 1;
-                b++;
-            }
-
         }
 
         return new int[]{0, 0};
