4c266d5856d8f9de106040af84bd46a23df4b1f2
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-05 20:39:13 +0900

    [level 2] Title: 괄호 회전하기, Time: 11.66 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,38 +2,38 @@ import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
-        if (s.length() % 2 != 0) {
-            return 0;
-        }
-
         int count = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (i > 0) {
-                s = s.substring(1) + s.substring(0, 1);
-            }
-
-            Stack<Character> stack = new Stack<>();
-            for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-                if (stack.isEmpty()) {
-                    stack.add(c);
-                } else if (stack.peek() == '(' && c == ')') {
-                    stack.pop();
-                } else if (stack.peek() == '{' && c == '}') {
-                    stack.pop();
-                } else if (stack.peek() == '[' && c == ']') {
-                    stack.pop();
-                } else {
-                    stack.add(c);
-                }
-            }
-
-            if (stack.isEmpty()) {
+            if (right(s)) {
                 count++;
             }
+            s = s.substring(1) + s.charAt(0);
         }
 
         return count;
+    }
+
+    private boolean right(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            if (stack.peek() == '(' && c == ')') {
+                stack.pop();
+            } else if (stack.peek() == '{' && c == '}') {
+                stack.pop();
+            } else if (stack.peek() == '[' && c == ']') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
