a8af74dbbd525a36d8414d1c561846b2eb335003
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-05 09:27:59 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,16 +1,12 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution {
+    public int solution(int n, int a, int b) {
         int count = 0;
-        while (true) {
+        while (a != b) {
             a = a / 2 + a % 2;
             b = b / 2 + b % 2;
             count++;
-
-            if (a == b) {
-                return count;
-            }
         }
+
+        return count;
     }
 }
