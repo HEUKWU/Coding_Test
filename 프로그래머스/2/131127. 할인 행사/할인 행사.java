31891ec66a37ae0580ec445d827642f7685a2de6
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-05 21:13:08 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,34 +3,27 @@ import java.util.Map;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < want.length; i++){
-            map.put(want[i], number[i]);
-        }
-
         int count = 0;
-        for(int i = 0; i < discount.length - 10 + 1; i++){
-            Map<String, Integer> dMap = new HashMap<>();
 
-            for(int j = 0; j < 10; j++){
-                dMap.put(discount[i + j], dMap.getOrDefault(discount[i + j], 0) + 1);
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            for (int k = 0; k < want.length; k++) {
+                map.put(want[k], number[k]);
             }
 
-            boolean b = true;
-
-            for(String key : map.keySet()){
-                if(!map.get(key).equals(dMap.get(key))){
-                    b = false;
-                    break;
+            for (int j = i; j < 10 + i; j++) {
+                String dis = discount[j];
+                if (map.containsKey(dis)) {
+                    map.put(dis, map.get(dis) - 1);
+                    if (map.get(dis) == 0) {
+                        map.remove(dis);
+                    }
                 }
             }
 
-            if (!b) {
-                continue;
+            if (map.isEmpty()) {
+                count++;
             }
-
-            count++;
         }
 
         return count;
