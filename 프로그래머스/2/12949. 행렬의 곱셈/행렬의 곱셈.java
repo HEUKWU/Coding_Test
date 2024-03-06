ec0c57c80b8ec649babf1a8fb8c90a0e0b6ed6ee
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-06 22:35:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.57 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,7 +4,7 @@ class Solution {
 
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
-                for (int k = 0; k < arr1[0].length; k++) {
+                for (int k = 0; k < arr2.length; k++) {
                     a[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
