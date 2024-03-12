f2965623fdb756fc25657d7c7f83cf32457bd570
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-12 11:05:28 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
+        
         int i = 0;
-
         for (int j = 0; j < number.length() - k; j++) {
             int max = 0;
             for (int l = i; l <= k + j; l++) {
@@ -12,6 +12,7 @@ class Solution {
                     i = l + 1;
                 }
             }
+
             sb.append(max);
         }
 
