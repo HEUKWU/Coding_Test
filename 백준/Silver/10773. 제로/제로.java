a9011acaf8e4bec1832eecb1ac0ddf468fce843c
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-18 11:15:28 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23980 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            int a = Integer.parseInt(br.readLine());
+
+            if (a == 0) {
+                stack.pop();
+            } else {
+                stack.push(a);
+            }
+        }
+
+        for (Integer i : stack) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
