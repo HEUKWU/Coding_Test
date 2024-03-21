90e2b42eec2162b5a75f8a61b21b0bca9965f661
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-21 16:25:30 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,27 @@
 class Solution {
 
-    private static int count = 0;
-    private static boolean[] visited;
+    private int[][] dungeons;
+    private boolean[] visited;
+    private int count = 0;
 
-    private void dfs(int x, int y, int[][] dungeons) {
+    public int solution(int k, int[][] dungeons) {
+        this.dungeons = dungeons;
+        visited = new boolean[dungeons.length];
+
+        dfs(0, k);
+
+        return count;
+    }
+
+    private void dfs(int depth, int k) {
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i]) {
-                if (y >= dungeons[i][0]) {
-                    visited[i] = true;
-                    dfs(x + 1, y - dungeons[i][1], dungeons);
-                    visited[i] = false;
-                }
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(depth + 1, k - dungeons[i][1]);
+                visited[i] = false;
             }
         }
 
-        count = Math.max(count, x);
-    }
-
-    public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons);
-
-        return count;
+        count = Math.max(count, depth);
     }
 }
