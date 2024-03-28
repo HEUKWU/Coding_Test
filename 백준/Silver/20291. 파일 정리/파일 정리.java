166d82c7e6819b16544dee88d60c13da0aefca30
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-28 17:48:41 +0900

    [Silver III] Title: 파일 정리, Time: 812 ms, Memory: 67048 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer str = new StringTokenizer(br.readLine(),".");
+        int n = Integer.parseInt(str.nextToken());
 
         Map<String, Integer> map = new TreeMap<>();
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine().split("\\.")[1];
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            str = new StringTokenizer(br.readLine(),".");
+            str.nextToken();
+            String file = str.nextToken();
+            map.put(file, map.getOrDefault(file, 0) + 1);
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder file = new StringBuilder();
         for (String s : map.keySet()) {
-            sb.append(s).append(" ").append(map.get(s)).append('\n');
+            file.append(s + " " + map.get(s) + "\n");
         }
-
-        System.out.println(sb);
+        System.out.println(file);
     }
 }
