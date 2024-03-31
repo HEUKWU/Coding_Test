90c874d3c35fd10d01dc93093eb556bea44faf99
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-03-31 19:38:38 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 136 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -16,26 +16,26 @@ public class Main {
             board[i] = br.readLine();
         }
 
-        int count = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < row - 7; i++) {
             for (int j = 0; j < col - 7; j++) {
-                count = Math.min(count, method(i, j, board));
+                min = Math.min(min, method(i, j, board));
             }
         }
 
-        System.out.println(count);
+        System.out.println(min);
     }
 
     private static int method(int i, int j, String[] board) {
-        String[] answer = {"WBWBWBWB", "BWBWBWBW"};
+        String[] org = {"WBWBWBWB", "BWBWBWBW"};
         int count = 0;
 
         for (int k = 0; k < 8; k++) {
             int a = i + k;
             for (int l = 0; l < 8; l++) {
                 int b = j + l;
-
-                if (board[a].charAt(b) != answer[a % 2].charAt(l)) {
+                if (board[a].charAt(b) != org[k % 2].charAt(l)) {
                     count++;
                 }
             }
