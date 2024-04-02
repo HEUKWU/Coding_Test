3ad63df076ad7c5072e9da10fe220646ed15ac16
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-04-02 12:18:34 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int switchCount;
+    private static int[] switches;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        switchCount = Integer.parseInt(br.readLine());
+
+        switches = new int[switchCount];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < switchCount; i++) {
+            switches[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int studentsCount = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < studentsCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gender = Integer.parseInt(st.nextToken());
+            int number = Integer.parseInt(st.nextToken());
+
+            if (gender == 1) {
+                man(number);
+            } else {
+                woman(number);
+            }
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < switchCount; i++) {
+            sb.append(switches[i]).append(' ');
+            if ((i + 1) % 20 == 0) {
+                sb.append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void man(int num) {
+        for (int j = 0; j < switchCount; j++) {
+            if ((j + 1) % num == 0)
+                switches[j] = switches[j] == 0 ? 1 : 0;
+        }
+    }
+
+    private static void woman(int num) {
+        switches[num - 1] = switches[num - 1] == 0 ? 1 : 0;
+        for (int i = 1; i < switchCount / 2; i++) {
+            if (num - 1 + i >= switchCount || num - 1 - i < 0)
+                break;
+            if (switches[num - 1 - i] == switches[num - 1 + i]) {
+                switches[num - 1 - i] = switches[num - 1 - i] == 0 ? 1 : 0;
+                switches[num - 1 + i] = switches[num - 1 + i] == 0 ? 1 : 0;
+            } else {
+                break;
+            }
+        }
+    }
+}
