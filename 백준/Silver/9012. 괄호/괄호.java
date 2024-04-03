777b8cb13339488f268148a24d5f23c32367666d
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-04-03 10:31:56 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,54 +4,35 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-
-    private static final StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             String parenthesis = br.readLine();
-            
-            check(parenthesis);
+            sb.append(check(parenthesis) ? "YES" : "NO").append('\n');
         }
 
         System.out.println(sb);
     }
 
-    private static void check(String parenthesis) {
+    private static boolean check(String parenthesis) {
         Stack<Character> stack = new Stack<>();
-        char first = parenthesis.charAt(0);
 
-        if (first == ')') {
-            sb.append("NO").append('\n');
-            return;
-        }
-
-        stack.add(first);
-
-        for (int i = 1; i < parenthesis.length(); i++) {
+        for (int i = 0; i < parenthesis.length(); i++) {
             char c = parenthesis.charAt(i);
 
-            if (stack.isEmpty()) {
-                stack.add(c);
-                continue;
-            }
-
-            if (stack.peek() == '(' && c == ')') {
-                stack.pop();
+            if (c == '(') {
+                stack.push(c);
             } else {
-                stack.add(c);
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
             }
         }
 
-        if (stack.isEmpty()) {
-            sb.append("YES");
-        } else {
-            sb.append("NO");
-        }
-
-        sb.append('\n');
+        return stack.isEmpty();
     }
 }
