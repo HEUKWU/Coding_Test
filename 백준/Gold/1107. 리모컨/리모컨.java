fac8aa3183c8734c5fc8846b2dc452ee6efd12eb
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-04-10 16:54:47 +0900

    [Gold V] Title: 리모컨, Time: 324 ms, Memory: 64996 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,15 +1,20 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
         boolean[] broken = new boolean[10];
-        for (int i = 0; i < m; i++) {
-            broken[sc.nextInt()] = true;
+        if (m != 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                broken[Integer.parseInt(st.nextToken())] = true;
+            }
         }
 
         if (n == 100) {
