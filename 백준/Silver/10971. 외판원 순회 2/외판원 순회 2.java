e4f305fd5981ac269549b602480fe3e8cdb39c59
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-04-10 18:15:53 +0900

    [Silver II] Title: 외판원 순회 2, Time: 128 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -2,7 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+/*
+메모리: 14616 시간: 168
+---------------------------------------
+백트래킹
+1부터 N까지 경로 다 더했을 때 최소 비용
+출발 지점 정해서 모든 경로 계산
+마지막에 되돌아오는 경로까지 계산했을 때의 최솟값
+ */
 public class Main {
 
     private static int n;
@@ -34,7 +41,12 @@ public class Main {
     }
 
     private static void dfs(int start, int now, int depth, int sum) {
+        if (sum > answer) {
+            return;
+        }
+        
         if (depth == n - 1) {
+            // 다 더했을 때 되돌아오는 길이 존재하면 비용 최솟값으로 갱신
             int returnToStart = arr[now][start];
             if (returnToStart != 0) {
                 answer = Math.min(answer, sum + returnToStart);
@@ -42,6 +54,7 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
+            // 아직 가지 않았고 길이 존재할 때
             if (!visited[i] && arr[now][i] != 0) {
                 visited[i] = true;
                 dfs(start, i, depth + 1, sum + arr[now][i]);
