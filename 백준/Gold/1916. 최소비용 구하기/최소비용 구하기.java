f31a1b4002766a43d43abcf92deedfa892d70009
Heukwu <100930333+HEUKWU@users.noreply.github.com>
2024-04-12 15:24:39 +0900

    [Gold V] Title: 최소비용 구하기, Time: 556 ms, Memory: 50772 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -6,68 +6,77 @@ import java.util.*;
 public class Main {
 
     private static final int INF = (int) 1e9;
-    private static final List<List<Bus>> graph = new ArrayList<>();
+    private static int n;
+    private static final List<List<Bus>> list = new ArrayList<>();
     private static int[] d;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-
-        d = new int[n + 1];
-
-        Arrays.fill(d, INF);
+        n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
 
+        d = new int[n + 1];
+        Arrays.fill(d, INF);
+
+        int m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int fare = Integer.parseInt(st.nextToken());
 
-            graph.get(start).add(new Bus(end, fare));
+            list.get(start).add(new Bus(end, fare));
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int departure = Integer.parseInt(st.nextToken());
         int arrival = Integer.parseInt(st.nextToken());
 
-        System.out.println(dijkstra(departure, arrival));
+        dijkstra(departure);
+
+        System.out.println(d[arrival]);
     }
 
-    private static int dijkstra(int departure, int arrival) {
+    private static void dijkstra(int departure) {
         PriorityQueue<Bus> q = new PriorityQueue<>();
-        q.offer(new Bus(departure, 0));
+        q.add(new Bus(departure, 0));
         d[departure] = 0;
 
         while (!q.isEmpty()) {
             Bus bus = q.poll();
-            int end = bus.end;
-            int fare = bus.fare;
+            int end = bus.getEnd();
+            int fare = bus.getFare();
 
             if (d[end] < fare) {
                 continue;
             }
 
-            for (int i = 0; i < graph.get(end).size(); i++) {
-                fare = d[end] + graph.get(end).get(i).fare;
+            for (Bus b : list.get(end)) {
+                fare = d[end] + b.getFare();
 
-                if (fare < d[graph.get(end).get(i).end]) {
-                    d[graph.get(end).get(i).end] = fare;
-                    q.offer(new Bus(graph.get(end).get(i).end, fare));
+                if (fare < d[b.getEnd()]) {
+                    d[b.getEnd()] = fare;
+                    q.offer(new Bus(b.getEnd(), fare));
                 }
             }
         }
-
-        return d[arrival];
     }
 
     private static class Bus implements Comparable<Bus> {
         private final int end;
         private final int fare;
+
+        public int getEnd() {
+            return end;
+        }
+
+        public int getFare() {
+            return fare;
+        }
 
         public Bus(int end, int fare) {
             this.end = end;
@@ -80,3 +89,4 @@ public class Main {
         }
     }
 }
+
